555cbb8252e753df07e3ac62d4934ab81f97c91f
Sorin <sorin@q3.ro>
2023-02-06 15:44:49 +0200

    change name

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button style={{background: '#c0c0c0'}}>{props.label}</button>;
 };
 
 export default Button;
