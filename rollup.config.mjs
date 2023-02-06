5212d60b4d8f8919d4d421177871b34b3bfb912a
Sorin <sorin@q3.ro>
2023-02-06 19:39:30 +0200

    rename

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -9,7 +9,7 @@ import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
-    input: "src/index.ts",
+    input: "src/index.js",
     output: [
       {
         file: packageJson.main,
